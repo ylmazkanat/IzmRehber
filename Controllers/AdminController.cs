1ec85c728c935cdcf3ba4b6d449d73deddc8248c
Yılmaz KANAT <yilmazkanat864@gmail.com>
2024-07-27 17:17:44 +0300

    Hataların giderilmesi ve düzenleme

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,33 +1,33 @@
 ﻿using IzmRehber.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace IzmRehber.Controllers
 {
     public class AdminController : Controller
     {
         private readonly AppDbContext _context;
+
         public AdminController(AppDbContext context)
         {
             _context = context;
         }
-        // GET: Dashborad
+
+        // GET: Dashboard
         public async Task<IActionResult> IndexAsync()
         {
-
             var employees = await _context.users.ToListAsync();
-
-
             return View(employees);
         }
 
         [HttpPost]
-        public IActionResult UpdateEmployee(int id, string adSoyad, string departman, string email, string tel)
+        public async Task<IActionResult> UpdateEmployee(int id, string adSoyad, string departman, string email, string tel)
         {
-            var employee = _context.users.FirstOrDefault(e => e.Id == id);
+            var employee = await _context.users.FirstOrDefaultAsync(e => e.Id == id);
             if (employee == null)
             {
-                return Json(new { success = false, message = "Employee not found" });
+                return Json(new { success = false, message = "Güncelleme Başarısız. Tekrar Deneyin." });
             }
 
             employee.AdSoyad = adSoyad;
@@ -35,8 +35,15 @@ namespace IzmRehber.Controllers
             employee.Email = email;
             employee.Tel = tel;
 
-            _context.SaveChanges();
-            return Json(new { success = true, message = "Employee updated successfully" });
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Json(new { success = true, message = "Çalışan Güncellendi." });
+            }
+            catch (DbUpdateException ex)
+            {
+                return Json(new { success = false, message = "Veritabanı güncellemesi sırasında bir hata oluştu: " + ex.Message });
+            }
         }
     }
 }
