27ac7bcf1f71574e4e2256cc3acf0fe793519428
Yılmaz KANAT <yilmazkanat864@gmail.com>
2024-07-28 21:04:10 +0300

    Hataların Düzeltilmesi, Güvenlik ayarları, Giriş ayarları

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using IzmRehber.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace IzmRehber.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dashboard
+        public async Task<IActionResult> Index()
+        {
+            // Oturum kontrolü
+            if (HttpContext.Session.GetInt32("UserId") == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            var employees = await _context.users.ToListAsync();
+            return View(employees);
+        }
+    }
+}
